a55094d035aedf2152e28d01b4e1218c9a6587c7
Faisal IDH <mfrizki1@gmail.com>
2025-01-26 16:42:50 +0700

    Added toolbar & navigation in MainActivity

--- app/src/main/java/id/calocallo/loanapp/MainActivity.kt
+++ app/src/main/java/id/calocallo/loanapp/MainActivity.kt
@@ -1,20 +1,59 @@
 package id.calocallo.loanapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import id.calocallo.loanapp.databinding.ActivityMainBinding
+import id.calocallo.loanapp.utils.viewBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        setupNavHost()
+
+    }
+
+    private fun setupNavHost() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcvLoan) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val inflater = navController.navInflater
+        val graph = inflater.inflate(R.navigation.navigation_loan)
+        navController.graph = graph
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setSupportActionBar(binding.toolbarBT)
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration,
+        )
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (!navController.popBackStack()) {
+                finish()
+            }
         }
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
